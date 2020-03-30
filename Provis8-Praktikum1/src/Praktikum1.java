e27b88504c9caa325be66a3bcc65cd31a84527f5
Rifqi Pratama <rifqipratama9@gmail.com>
2020-03-30 17:29:37 +0700

    Tugas-1 Rifqi Pratama

---
+++ Provis8-Praktikum1/src/Praktikum1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author oyeaaa
+ */
+public class Praktikum1 {
+    
+    public static void main(String[] args) {
+        Frame futama = new Frame();
+        futama.setTitle("Provis8-Praktikum1");
+        futama.setVisible(true);
+    }
+}
